d0e3fc881e3bcd611ebb92b6afee37b2cb1eb648
pacurtin <pacurtin@tcd.ie>
2017-09-17 10:44:41 +1000

    Added alarm manager functionality to change wallpaper once per minute

--- app/src/main/java/com/curtin/padraig/wisdomofsuntzu/MainActivity.java
+++ app/src/main/java/com/curtin/padraig/wisdomofsuntzu/MainActivity.java
@@ -1,6 +1,12 @@
 package com.curtin.padraig.wisdomofsuntzu;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.WallpaperManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,11 +14,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    BroadcastReceiver mReceiver;
+    ImageCreator imageCreator;
+    String[] quotes;
+    Bitmap newWallPaper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,36 +35,63 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final ImageCreator imageCreator = new ImageCreator(getApplicationContext());
-        final String[] quotes = getResources().getStringArray(R.array.sunTzuQuotes);
+        imageCreator = new ImageCreator(getApplicationContext());
+        quotes = getResources().getStringArray(R.array.sunTzuQuotes);
 
         FloatingActionButton refreshButton = (FloatingActionButton) findViewById(R.id.refreshButton);
         final ImageView image = (ImageView) findViewById(R.id.content_main_image);
 
-        refreshWallpaper(image,imageCreator, quotes);
+        newWallPaper = refreshWallpaper(imageCreator, quotes);
+        image.setImageBitmap(newWallPaper);
 
         refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                refreshWallpaper(image,imageCreator, quotes);
+                newWallPaper = refreshWallpaper(imageCreator, quotes);
+                image.setImageBitmap(newWallPaper);
             }
         });
+
+        RegisterAlarmBroadcast();
+        //long firstAlarmTime = 1000*60*60*5;//5AM in milliseconds
+        //long alarmInterval = 1000*60*60*24;//24Hrs in ms
+        long alarmInterval = 1000*60;
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), alarmInterval , pendingIntent);
     }
 
-    void refreshWallpaper(ImageView image, ImageCreator imageCreator, String[] quotes){
+    private void RegisterAlarmBroadcast() {
+        mReceiver = new BroadcastReceiver() {
+            // private static final String TAG = "Alarm Example Receiver";
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                refreshWallpaper(imageCreator, quotes);
+            }
+        };
+
+        registerReceiver(mReceiver, new IntentFilter("sample"));
+        pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent("sample"), 0);
+        alarmManager = (AlarmManager)(this.getSystemService(Context.ALARM_SERVICE));
+    }
+
+    private void UnregisterAlarmBroadcast() {
+        alarmManager.cancel(pendingIntent);
+        getBaseContext().unregisterReceiver(mReceiver);
+    }
+
+    Bitmap refreshWallpaper(ImageCreator imageCreator, String[] quotes){
+        // Overlays a random string from a string array over a black bitmap and sets it as the phones wallpaper. Returns the bitmap too.
         Random random = new Random();
         int randomQuoteNum = random.nextInt(quotes.length);
         String quote = quotes[randomQuoteNum];
         Bitmap wallpaper = imageCreator.addTextToImageBitmap(quote);
-        image.setImageBitmap(wallpaper);
         WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
         try{
             wallpaperManager.setBitmap(wallpaper);
         }catch (IOException ex){
             ex.printStackTrace();
         }
+
+        return wallpaper;
     }
-
-
 
 }
